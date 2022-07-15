6bcf96fc2b77b7188a5d24c27a40a2b48da6648c
Bodó Regina <80204216+Jenga05@users.noreply.github.com>
2022-07-15 16:35:51 +0200

    Add files via upload

---
+++ feladat01.js
@@ -0,0 +1,35 @@
+function feladat01(szinek) {
+    let number = szinek.length;
+    let sum = 0;
+    let top;
+    let bottom;
+    let fraction;
+  
+    for (let i = 2; i < number; i++) {
+  
+      number = szinek.length;
+      top = 1;
+      bottom = 1;
+  
+      for (let j = 0; j < i; j++) {
+        if (number > j){
+          top *= (number - j);
+        }
+        if (j != 0)
+          bottom *= (j+1);
+  
+      }
+  
+      fraction = top / bottom;
+      sum += fraction;
+  
+    }
+  
+    sum++;
+  
+    if (szinek.length < 2 || sum === undefined) {
+      sum = 0;
+    }
+  
+  }
+  
